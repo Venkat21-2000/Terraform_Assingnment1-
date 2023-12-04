7eef191737e8c0d218c87b527e7212761a0a8cc6
Venkat21-2000 <106486649+Venkat21-2000@users.noreply.github.com>
2023-12-04 19:44:51 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "public_instance_ip" {
-  value = module.ec2.public_instance_ip
+  value = module.ec2.public_instance_id
 }
 
 output "private_instance_ip" {
-  value = module.ec2.private_instance_ip
+  value = module.ec2.private_instance_id
 }
